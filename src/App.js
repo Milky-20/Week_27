6ea8a9417065534eaa6606001a6b15b652b52d7d
milena <katyawey2002@mail.ru>
2023-02-12 21:12:48 +0300

    color

--- src/App.js
+++ src/App.js
@@ -5,13 +5,13 @@ import Cards from './components/Cards'
 
 const cardss = [
   {
-    name: "Безлимитный 300", price: "300", priceTextHigh: "руб", priceTextLow: "/мес", speed: "до 10 Мбит/сек", condition: "Объем включенного трафика не ограничен", color: "#42aaff",
+    name: "Безлимитный 300", price: "300", priceTextHigh: "руб", priceTextLow: "/мес", speed: "до 10 Мбит/сек", condition: "Объем включенного трафика не ограничен", color: "#7fc7ff",
   },
   {
-    name: "Безлимитный 450", price: "450", priceTextHigh: "руб", priceTextLow: "/мес", speed: "до 50 Мбит/сек", condition: "Объем включенного трафика не ограничен", color: "#008000",
+    name: "Безлимитный 450", price: "450", priceTextHigh: "руб", priceTextLow: "/мес", speed: "до 50 Мбит/сек", condition: "Объем включенного трафика не ограничен", color: "#00a550",
   },
   {
-    name: "Безлимитный 500", price: "500", priceTextHigh: "руб", priceTextLow: "/мес", speed: "до 100 Мбит/сек", condition: "Объем включенного трафика не ограничен", isSelected: true, color: "#ff0000",
+    name: "Безлимитный 500", price: "500", priceTextHigh: "руб", priceTextLow: "/мес", speed: "до 100 Мбит/сек", condition: "Объем включенного трафика не ограничен", isSelected: true, color: "#ff0033",
   },
   {
     name: "Безлимитный 1000", price: "1000", priceTextHigh: "руб", priceTextLow: "/мес", speed: "до 200 Мбит/сек", condition: "Объем включенного трафика не ограничен", color: "#16251c",
