363954ac3cf3acdddb6f515c0ee1d88573d1aee2
milena <katyawey2002@mail.ru>
2023-02-13 12:53:41 +0300

    стили

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ const cardss = [
     name: "Безлимитный 450", price: "450", priceTextHigh: "руб", priceTextLow: "/мес", speed: "до 50 Мбит/сек", condition: "Объем включенного трафика не ограничен", color: "#00a550",
   },
   {
-    name: "Безлимитный 500", price: "500", priceTextHigh: "руб", priceTextLow: "/мес", speed: "до 100 Мбит/сек", condition: "Объем включенного трафика не ограничен", isSelected: true, color: "#ff0033",
+    name: "Безлимитный 550", price: "550", priceTextHigh: "руб", priceTextLow: "/мес", speed: "до 100 Мбит/сек", condition: "Объем включенного трафика не ограничен", isSelected: true, color: "#ff0033",
   },
   {
     name: "Безлимитный 1000", price: "1000", priceTextHigh: "руб", priceTextLow: "/мес", speed: "до 200 Мбит/сек", condition: "Объем включенного трафика не ограничен", color: "#16251c",
